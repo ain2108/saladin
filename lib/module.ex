8bec5d91aa5e1b1a6e82449e7aede2641c1611bc
ain2108 <ain2108@columbia.edu>
2020-07-02 14:23:14 -0400

    seems to do what I want it to do using the method injection

--- lib/module.ex
+++ lib/module.ex
@@ -1,46 +1,60 @@
 defmodule Saladin.Module do
-  @spec start_link(any) :: {:ok, pid}
-  def start_link(ports) do
-    # Want to make sure our simulation crashes in case a process crashes
-    Task.start_link(fn -> reset(%{:ports => ports}) end)
-  end
 
-  defp reset state do
-    IO.puts("reset")
-    wait(state)
-    IO.puts("received first clock pulse")
-    loop(state)
-  end
+  @callback reset(Map.t) :: {:ok, term} | {:error, String.t}
+  @callback loop(Map.t) :: any
 
-  defp wait state, timeout \\ 10_000 do
-    clock_pid = state[:ports][:clock]
+  # When you call use in your module, the __using__ macro is called.
+  defmacro __using__(_params) do
+    quote do
+      # User modules must implement the Saladin.Module callbacks
+      @behaviour Saladin.Module
 
-    # Tell the clock you are ready for the next cycle
-    send(clock_pid, {:ready, self()})
+      # Define implementation for user modules to use
+      def reset(state), do: {:error, "not implemented"}
+      def loop(state), do: {:error, "not implemented"}
 
-    receive do
-      {:tiktok} -> IO.puts("tiktok")
-      {:reset} -> reset(state)
-    after
-      timeout ->
-        Process.exit(self(), "no clock signal has been received")
+      def start_link(ports) do
+        # Want to make sure our simulation crashes in case a process crashes
+        Task.start_link(fn -> reset_sequence(%{:ports => ports}) end)
+      end
+      def reset(_state) do
+        IO.puts("default reset sequence")
+      end
+
+      def loop(state) do
+        IO.puts("default loop")
+        wait(state)
+        loop(state)
+      end
+
+      defp reset_sequence state do
+        IO.puts("reset_sequence")
+        reset(state)
+        wait(state)
+        IO.puts("received first clock pulse")
+        loop(state)
+      end
+
+      defp wait(state, timeout \\ 10_000) do
+        clock_pid = state[:ports][:clock]
+
+        # Tell the clock you are ready for the next cycle
+        send(clock_pid, {:ready, self()})
+
+        receive do
+          {:tiktok} -> IO.puts("tiktok")
+          {:reset_sequence} -> reset_sequence(state)
+        after
+          timeout ->
+            Process.exit(self(), "no clock signal has been received")
+        end
+      end
+
+      # Defoverridable makes the given functions in the current module overridable
+      # Without defoverridable, new definitions of greet will not be picked up
+      defoverridable [reset: 1, loop: 1]
     end
   end
 
-  defp loop(state) do
-    # Main compute loop
 
-    # Block on
-    receive do
-      {:hello} -> IO.puts("Hello world!")
-    after
-      0 -> :ok
-    end
-
-    # Wait for the next clock cycle
-    wait(state)
-
-    # Get back into the loop
-    loop(state)
-  end
 end
--- test/saladin_module_test.exs
+++ test/saladin_module_test.exs
@@ -1,11 +1,47 @@
+defmodule TestModule do
+  use Saladin.Module
+
+  @impl true
+  def reset(state) do
+    IO.puts("SimpleModule reset")
+  end
+
+  @impl true
+  def loop(state) do
+    # Main compute loop
+
+    # Block on
+    receive do
+      {:hello} -> IO.puts("TestModule received the hello")
+    after
+      0 -> :ok
+    end
+
+    # Wait for the next clock cycle
+    Saladin.Module.wait(state)
+
+    # Get back into the loop
+    loop(state)
+  end
+
+end
+
 defmodule Saladin.Module.Test do
   use ExUnit.Case
   doctest Saladin.Module
 
-  test "mod sends :ready after reset" do
+  test "module sends :ready after reset" do
     clock_pid = self()
-    {:ok, pid} = Saladin.Module.start_link(%{:clock => clock_pid})
+    {:ok, pid} = TestModule.start_link(%{:clock => clock_pid})
     assert_receive {:ready, mod_pid}, 5_000
     assert pid == mod_pid
   end
+
+  # test "module progresses after receiving :tiktok" do
+  #   clock_pid = self()
+  #   {:ok, pid} = Saladin.Module.start_link(%{:clock => clock_pid})
+  #   assert_receive {:ready, mod_pid}, 5_000
+  #   send pid, {:tiktok}
+  #   assert_receive {:ready, mod_pid}, 5_000
+  # end
 end
