4b50057a8dae98e4fcac6114e89d5ffe18d9c760
ain2108 <ain2108@columbia.edu>
2020-07-04 23:31:04 -0400

    a basic cycle accurate test, seems to work

--- lib/impls/arbitrated_scratchpad.ex
+++ lib/impls/arbitrated_scratchpad.ex
@@ -16,19 +16,26 @@ defmodule Saladin.Impls.ArbitratedScratchpad do
     # uncommenting this removes the dyalizer error
     tick_number = state.tick_number
 
+    # IO.puts("#{tick_number}")
+
     # We want to read the request sent on previous clock cycle
     state =
       receive do
         {:read, addr, pid, req_tick_number} when req_tick_number < tick_number ->
           [{_, value}] = :ets.lookup(state.plm, addr)
+          # IO.puts("read: #{addr}:#{value} from #{req_tick_number} executed at #{tick_number}")
           # clock cycle to read from PLM
           state = wait(state)
+          # IO.puts("read: to request register: #{addr}:#{value} at #{state.tick_number}")
           send(pid, {:read_done, addr, value, state.tick_number})
           state
 
         {:write, addr, value, pid, req_tick_number} when req_tick_number < tick_number ->
+          # IO.puts("write: #{addr}:#{value} from #{req_tick_number} executed at #{tick_number}")
           :ets.insert(state.plm, {addr, value})
           state = wait(state)
+
+          # IO.puts("read: returning to request register: #{addr}:#{value} at #{state.tick_number}")
           send(pid, {:write_done, addr, value, state.tick_number})
           state
       after
--- test/saladin_impls_test.exs
+++ test/saladin_impls_test.exs
@@ -2,8 +2,35 @@ defmodule Saladin.Impls.ArbitratedScratchpadTest do
   use ExUnit.Case
 
   test "basic implementation test" do
+    # for registration
     {:ok, clock_pid} = Saladin.Clock.start_link(%{})
-    {:ok, module_pid} = Saladin.Impls.ArbitratedScratchpad.start_link(%{clock: clock_pid})
+    plm_config = %{nbanks: 1, bank_size: 516}
+
+    {:ok, module_pid} =
+      Saladin.Impls.ArbitratedScratchpad.start_link(%{clock: clock_pid, plm_config: plm_config})
+
     Saladin.Utils.wait_for_state(clock_pid, &MapSet.member?(&1[:modules], module_pid))
+
+    # wait() in reset, Scratchpad check #0, no work
+    send(module_pid, {:tick, 0})
+    # Scratchpad check #1, no work
+    send(module_pid, {:tick, 1})
+
+    send(module_pid, {:write, 17, 13, self(), 1})
+    # Scratchpad check #2, need to do write
+    send(module_pid, {:tick, 2})
+    # Scratchpad check #3, PLM write complete, send confirmation
+    send(module_pid, {:tick, 3})
+
+    send(module_pid, {:read, 17, self(), 3})
+    # to consumer only visible on cycle #4
+    assert_receive {:write_done, 17, 13, 3}
+
+    # Scratchpad check #4, need to do read
+    send(module_pid, {:tick, 4})
+    # Scratchpad check #5, PLM read and send back
+    send(module_pid, {:tick, 5})
+    # to consumer only visible on cycle #6
+    assert_receive {:read_done, 17, 13, 5}
   end
 end
