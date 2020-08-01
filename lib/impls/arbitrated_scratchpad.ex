9aea0db808b8d62ea30797ff713387ce759ef3f7
Anton Nefedenkov <ain2108@columbia.edu>
2020-08-01 11:47:20 -0400

    Anton/basic scratchpad impl (#3)
    * implemented module registration
    
    * implemented round robin
    
    * added basic simulation

--- lib/impls/arbitrated_scratchpad.ex
+++ lib/impls/arbitrated_scratchpad.ex
@@ -1,50 +1,90 @@
 defmodule Saladin.Impls.ArbitratedScratchpad do
   use Saladin.Module
 
+  defp wait_consumer_registration(state, cur, num_consumers) do
+    # Register consumers
+    if cur < num_consumers do
+      receive do
+        {:register_consumer, consumer_pid, consumer_input} ->
+          send(consumer_pid, {:register_consumer_ok, state.input})
+
+          # Add the consumer_pid to the array of consumers
+          Map.update!(state, :consumers, &(&1 |> Map.put(cur, consumer_input)))
+          |> wait_consumer_registration(cur + 1, num_consumers)
+      after
+        5_000 ->
+          Process.exit(
+            self(),
+            "Missing consumer registration. Make sure all scratchpad consumers send :register_consumer"
+          )
+      end
+    else
+      state
+    end
+  end
+
   def reset(state) do
     plm = :ets.new(:buckets_registry, [:set, :private])
+
+    plm_init = Map.get(state.plm_config, :plm_init, [])
 
     # Initialize the PLM
     for addr <- 0..(state.plm_config.nbanks * state.plm_config.bank_size - 1),
         do: :ets.insert(plm, {addr, 0})
 
-    state |> Map.put(:plm, plm)
+    # Update PLM with init values
+    for addr_value <- plm_init do
+      :ets.insert(plm, addr_value)
+    end
+
+    # Wait for all consumers to register
+    state = state |> Map.put(:consumers, %{}) |> Map.put(:plm, plm) |> Map.put(:cur_consumer_i, 0)
+    wait_consumer_registration(state, 0, state.num_consumers)
   end
 
   def run(state) do
     # Check if there is a
-    # uncommenting this removes the dyalizer error
     tick_number = state.tick_number
-
-    # IO.puts("#{tick_number}")
+    cur_consumer = state.consumers[state.cur_consumer_i]
 
     # We want to read the request sent on previous clock cycle
     state =
       receive do
-        {:read, addr, pid, req_tick_number} when req_tick_number < tick_number ->
-          [{_, value}] = :ets.lookup(state.plm, addr)
-          # IO.puts("read: #{addr}:#{value} from #{req_tick_number} executed at #{tick_number}")
+        # Notice the strict <, req_tick_number is the cycle on which the consumer was driving register inputs
+        # req_tick_number + 1 is the earliest cycle when request register value is available to the arbitrator
+        {:read, addr, pid, req_tick_number}
+        when req_tick_number < tick_number and pid == cur_consumer ->
           # clock cycle to read from PLM
           state = wait(state)
-          # IO.puts("read: to request register: #{addr}:#{value} at #{state.tick_number}")
-          # TODO: Might wanna send the state.tick_number + 1, i.e the tick when the value is actually available.
-          Saladin.Module.Input.drive(pid, {:read_done, addr, value, state.tick_number})
+          [{_, value}] = :ets.lookup(state.plm, addr)
+
+          # Produce the output, only should become visible to consumer next clock cycle
+          Saladin.Module.Input.drive(pid, {:read_done, addr, value, state.tick_number + 1})
+          # clock cycle spent writing into output register
+          state = wait(state)
+
+          # Output signal driven by the output register
           state
 
-        {:write, addr, value, pid, req_tick_number} when req_tick_number < tick_number ->
+        {:write, addr, value, pid, req_tick_number}
+        when req_tick_number < tick_number and pid == cur_consumer ->
           # IO.puts("write: #{addr}:#{value} from #{req_tick_number} executed at #{tick_number}")
           :ets.insert(state.plm, {addr, value})
           state = wait(state)
 
+          # Produce the output, only should become visible to consumer next clock cycle
+          Saladin.Module.Input.drive(pid, {:write_done, addr, value, state.tick_number + 1})
+          # clock cycle spent writing into output register
+          state = wait(state)
+
           # IO.puts("write: returning to request register: #{addr}:#{value} at #{state.tick_number}")
-          Saladin.Module.Input.drive(pid, {:write_done, addr, value, state.tick_number})
           state
       after
-        0 -> state
+        # If no valid request, need to wait a clock cycle anyways
+        0 -> wait(state)
       end
 
-    state = wait(state)
-    run(state)
+    state |> Map.update!(:cur_consumer_i, &rem(&1 + 1, state.num_consumers)) |> run()
   end
 end
 
@@ -63,7 +103,7 @@ defmodule Saladin.Impls.ScratchpadConsumerInterface do
     tick_number = state.tick_number
 
     receive do
-      {:write_done, addr, value, req_tick_number} when req_tick_number < tick_number ->
+      {:write_done, addr, value, req_tick_number} when req_tick_number <= tick_number ->
         {state, {:write_done, addr, value, req_tick_number}}
     after
       0 ->
@@ -84,7 +124,7 @@ defmodule Saladin.Impls.ScratchpadConsumerInterface do
     tick_number = state.tick_number
 
     receive do
-      {:read_done, addr, value, req_tick_number} when req_tick_number < tick_number ->
+      {:read_done, addr, value, req_tick_number} when req_tick_number <= tick_number ->
         {state, {:read_done, addr, value, req_tick_number}}
     after
       0 ->
@@ -99,5 +139,13 @@ defmodule Saladin.Impls.ScratchpadConsumerInterface do
     # Wait a clock cycle
     state = Saladin.Utils.wait(state)
     wait_read(pid, state)
+  end
+
+  def register_consumer(scratchpad_pid, state) do
+    send(scratchpad_pid, {:register_consumer, self(), state.input})
+
+    receive do
+      {:register_consumer_ok, scratchpad_input} -> scratchpad_input
+    end
   end
 end
--- test/saladin_impls_test.exs
+++ test/saladin_impls_test.exs
@@ -4,9 +4,16 @@ defmodule Saladin.Impls.ArbitratedScratchpadTest do
   defmodule BasicConsumerModule do
     use Saladin.Module
 
+    def reset(state) do
+      scratchpad_input =
+        Saladin.Impls.ScratchpadConsumerInterface.register_consumer(state.scratchpad_pid, state)
+
+      state |> Map.put(:scratchpad_input, scratchpad_input)
+    end
+
     def run(state) do
       test_server = state.test_server
-      scratchpad_server = state.scratchpad_server
+      scratchpad_input = state.scratchpad_input
       req_start_tick_number = state.tick_number
 
       # Be very careful with state, must keep passing it forward
@@ -14,7 +21,7 @@ defmodule Saladin.Impls.ArbitratedScratchpadTest do
         receive do
           {:test_read, addr} ->
             {state, res} =
-              Saladin.Impls.ScratchpadConsumerInterface.read(scratchpad_server, addr, state)
+              Saladin.Impls.ScratchpadConsumerInterface.read(scratchpad_input, addr, state)
 
             send(test_server, {res, req_start_tick_number})
             state
@@ -22,7 +29,7 @@ defmodule Saladin.Impls.ArbitratedScratchpadTest do
           {:test_write, addr, value} ->
             {state, res} =
               Saladin.Impls.ScratchpadConsumerInterface.write(
-                scratchpad_server,
+                scratchpad_input,
                 addr,
                 value,
                 state
@@ -45,26 +52,28 @@ defmodule Saladin.Impls.ArbitratedScratchpadTest do
     # Initialize the ScratchPad
     plm_config = %{nbanks: 1, bank_size: 512}
 
-    {:ok, scratchpad_clock, scratchpad_input} =
-      Saladin.Impls.ArbitratedScratchpad.start_link(%{clock: clock_pid, plm_config: plm_config})
+    {:ok, scratchpad_pid, _} =
+      Saladin.Impls.ArbitratedScratchpad.start_link(%{
+        clock: clock_pid,
+        plm_config: plm_config,
+        num_consumers: 1
+      })
 
     # Start the scratchpad consumer
     {:ok, tester_pid, _} =
       BasicConsumerModule.start_link(%{
         clock: clock_pid,
         # Notice how we pass the input pid as opposed to clock pid
-        scratchpad_server: scratchpad_input,
+        scratchpad_pid: scratchpad_pid,
         test_server: self()
       })
 
     Saladin.Utils.wait_for_state(
       clock_pid,
-      &MapSet.equal?(&1[:modules], MapSet.new([tester_pid, scratchpad_clock]))
+      &MapSet.equal?(&1[:modules], MapSet.new([tester_pid, scratchpad_pid]))
     )
 
     Saladin.Clock.start_clock(clock_pid)
-
-    # assert false
 
     for test_addr <- 0..(bank_size - 1) do
       test_value = :rand.uniform(max_value)
@@ -79,7 +88,7 @@ defmodule Saladin.Impls.ArbitratedScratchpadTest do
       # min_op_latency measures the period until the value is available to the consumer.module
       # Since there is a single consumer,
       assert req_start_tick + Saladin.Impls.ScratchpadConsumerInterface.min_op_latency() ==
-               req_done_tick + 1
+               req_done_tick
 
       send(tester_pid, {:test_read, test_addr})
       assert_receive {{:read_done, read_addr, read_value, req_done_tick}, req_start_tick}
@@ -88,7 +97,140 @@ defmodule Saladin.Impls.ArbitratedScratchpadTest do
       assert read_value == value
 
       assert req_start_tick + Saladin.Impls.ScratchpadConsumerInterface.min_op_latency() ==
-               req_done_tick + 1
+               req_done_tick
     end
+  end
+end
+
+defmodule Saladin.Impls.Simulate do
+  use ExUnit.Case
+
+  defmodule BasicConsumerModule do
+    use Saladin.Module
+
+    def reset(state) do
+      scratchpad_input =
+        Saladin.Impls.ScratchpadConsumerInterface.register_consumer(state.scratchpad_pid, state)
+
+      state
+      |> Map.put(:scratchpad_input, scratchpad_input)
+      |> Map.put(:cur_addr, state.consumer_id)
+    end
+
+    defp do_work(state, cur_work_cycle, total_work_cycles)
+         when cur_work_cycle >= total_work_cycles do
+      state
+    end
+
+    defp do_work(state, cur_work_cycle, total_work_cycles)
+         when cur_work_cycle < total_work_cycles do
+      state = wait(state)
+      do_work(state, cur_work_cycle + 1, total_work_cycles)
+    end
+
+    defp spin(state) do
+      wait(state) |> spin()
+    end
+
+    def run(state) do
+      tester_pid = state.tester_pid
+      scratchpad_input = state.scratchpad_input
+      addr = state.cur_addr
+      total_consumers = state.total_consumers
+      work_cycles = state.work_cycles
+
+      # Read the value
+      read_start_tick = state.tick_number
+      {state, _} = Saladin.Impls.ScratchpadConsumerInterface.read(scratchpad_input, addr, state)
+
+      assert state.tick_number >=
+               read_start_tick + Saladin.Impls.ScratchpadConsumerInterface.min_op_latency()
+
+      # Simulate work for # work cycles
+      work_start_tick = state.tick_number
+      state = do_work(state, 0, work_cycles)
+      assert state.tick_number == work_start_tick + work_cycles
+
+      # Continue work if needed
+      if addr + total_consumers < state.total_work do
+        state = Map.update!(state, :cur_addr, &(&1 + total_consumers))
+        run(state)
+      else
+        IO.puts(:stderr, "consumer #{state.consumer_id} sending to tester")
+        send(tester_pid, {:consumer_done, state.consumer_id, state.tick_number})
+        IO.puts(:stderr, "consumer #{state.consumer_id} entering spin")
+        spin(state)
+      end
+    end
+  end
+
+  test "simulate consumption of RAS wrapped PLM" do
+    bank_size = 512
+    nbanks = 1
+    max_value = 65536
+    total_consumers = 1
+    total_work = bank_size * nbanks
+    work_cycles = 1
+
+    # Generate the list of values to populate the PLM with
+    plm_init = 0..(bank_size - 1) |> Enum.map(&{&1, :rand.uniform(max_value)})
+
+    # Start clock
+    {:ok, clock_pid} = Saladin.Clock.start_link(%{})
+
+    # Initialize the ScratchPad
+    plm_config = %{nbanks: nbanks, bank_size: bank_size, plm_init: plm_init}
+
+    {:ok, scratchpad_pid, _} =
+      Saladin.Impls.ArbitratedScratchpad.start_link(%{
+        clock: clock_pid,
+        plm_config: plm_config,
+        num_consumers: total_consumers
+      })
+
+    # Start the consumers
+    start_scratchpad_consumer = fn consumer_id ->
+      BasicConsumerModule.start_link(%{
+        clock: clock_pid,
+        scratchpad_pid: scratchpad_pid,
+        tester_pid: self(),
+        consumer_id: consumer_id,
+        total_consumers: total_consumers,
+        total_work: total_work,
+        work_cycles: work_cycles
+      })
+    end
+
+    consumers =
+      0..(total_consumers - 1)
+      |> Enum.map(fn consumer_id -> start_scratchpad_consumer.(consumer_id) end)
+
+    IO.puts(:stderr, "#{inspect(consumers)}")
+
+    # Wait for all modules to register with clock
+    Saladin.Utils.wait_for_state(
+      clock_pid,
+      &MapSet.equal?(
+        &1[:modules],
+        MapSet.new(
+          [scratchpad_pid] ++ Enum.map(consumers, fn {:ok, tester_pid, _} -> tester_pid end)
+        )
+      )
+    )
+
+    Saladin.Clock.start_clock(clock_pid)
+    IO.puts(:stderr, "clock started")
+
+    consumer_work_receipts =
+      1..total_consumers
+      |> Enum.map(fn _ ->
+        receive do
+          {:consumer_done, consumer_id, tick_number} ->
+            IO.puts(:stderr, "consumer #{consumer_id} done")
+            {consumer_id, tick_number}
+        end
+      end)
+
+    IO.puts(:stderr, "#{inspect(consumer_work_receipts)}")
   end
 end
