adcb32ca7174e68f190a00654766d9c34a92a17d
Anna <anefedenkova@oxy.edu>
2020-08-07 19:35:48 -0400

    new parsing

--- visualizer/main.py
+++ visualizer/main.py
@@ -47,10 +47,10 @@ class SheetRenderer:
 
     def __init__(self, service, sims):
         self.l_c_dict = {
-            "dark_yellow_1": [241, 194, 50],
-            "dark_green_1": [106, 168, 79],
-            "dark_cornflower_blue 1": [60, 120, 216],
-            "dark_red_1": [204, 0, 0],
+            "dark_yellow_1": (241, 194, 50),
+            "dark_green_1": (106, 168, 79),
+            "dark_cornflower_blue 1": (60, 120, 216),
+            "dark_red_1": (204, 0, 0),
             'dark_purple_1': [103, 78, 167],
             'dark_magenta_1': [166, 77, 121],
             'dark_cyan_1': [69, 129, 142],
@@ -58,9 +58,9 @@ class SheetRenderer:
         }
 
         self.d_c_dict = {
-            "dark_yellow_3": [127, 96, 0],
-            "dark_green_3": [39, 78, 19],
-            "dark_cornflower_blue 3": [28, 69, 135],
+            "dark_yellow_3": (127, 96, 0),
+            "dark_green_3": (39, 78, 19),
+            "dark_cornflower_blue 3": (28, 69, 135),
             'dark_red_3': [102, 0, 0],
             'dark_purple_3': [32, 18, 77],
             'dark_magenta_3': [76, 17, 48],
@@ -99,94 +99,131 @@ class SheetRenderer:
         self.sims = sims
         self.offset_row = 0
         self.offset_row_add = 0
-        self.row_index = 2
 
-    #in a simulation
-        self.arbiter_list_id = []
-        self.consumer_list_id = []
-        self.arbiter_count = 1
-        self.consumer_count = 1
-        self.d_color_count = 0
-        self.l_color_count = 0
 
-    def simulation_renderer(self):
-        for sim in self.sims:
-            SheetRenderer.reset_variables(self)
-            SheetRenderer.info_renderer(self, sim)
-            for i in range(0, len(sim)):
-                if isinstance(sim[i], ArbiterDataPoint):
-                    SheetRenderer.type_append_arb(self, sim[i])
-                    #SheetRenderer.color_arb_blocks(self, sim[i])
-                elif isinstance(sim[i], ConsumerDataPoint):
-                    SheetRenderer.type_append_con(self, sim[i])
-                    SheetRenderer.color_con_blocks(self, sim[i])
-            self.row_index += 4
+    # find and register all unique arbiters
 
-    #reset all the variables to initial position for each new simulation
+    def assign_arbiter(self, sim):
 
-    def reset_variables(self):
-        self.arbiter_list_id = []
-        self.consumer_list_id = []
-        self.arbiter_count = 1
-        self.consumer_count = 1
-        self.d_color_count = 0
-        self.l_color_count = 0
+        arbiters = {}
+        count = 0
 
-    #go through each line, define the type and append to a list of con/arb
+        for dp in sim:
+            if isinstance(dp, ArbiterDataPoint) and dp.arbiter_id in arbiters:
 
-    def type_append_arb(self, line):
-        if line.arbiter_id not in self.arbiter_list_id:
-            self.requests.append(input_data_into_cell((self.row_index), 0, "Arbiter" + str(self.arbiter_count)))
-            self.arbiter_list_id.append(line.arbiter_id)
-            self.arbiter_count += 1
-            self.row_index += 1
+                arbiters[dp.arbiter_id] = (dp.arbiter_id, count)
+                count += 1
 
-    def type_append_con(self, line):
-        if line.consumer_id not in self.consumer_list_id:
-            self.requests.append(input_data_into_cell((self.row_index), 0, "Consumer" + str(self.consumer_count)))
-            self.consumer_list_id.append(line.consumer_id)
-            self.consumer_count += 1
-            SheetRenderer.color_append_con(self, line)
-            self.row_index += 1
+        self.create_var_arb(arbiters)
+        return arbiters
 
-    #append a color to each con
+    #find and register all consumers and assign a color to each unique one
 
-    def color_append_con(self, line):
-        line.light_colors = list(self.l_c_dict.keys())[self.l_color_count]
-        line.dark_colors = list(self.d_c_dict.keys())[self.d_color_count]
-        self.d_color_count += 1
-        self.l_color_count += 1
+    def assign_colors_to_consumers(self, sim, arbiters):
+
+        ctcr = {}
+        count = 0
+
+        l_colors = list(self.l_c_dict.keys())
+        d_colors = list(self.d_c_dict.keys())
+
+        for dp in sim:
+            if isinstance(dp, ConsumerDataPoint) and dp.consumer_id not in ctcr:
+
+                l_color = l_colors[count]
+                d_color = d_colors[count]
+                ctcr[dp.consumer_id] = (l_color, d_color, count)
+
+                count += 1
+
+        self.create_var_con(ctcr)
+
+        return ctcr
+
+    #print the arb variables
+    #in order to do so, require to have an updated self.offset_row
+
+    def create_var_arb(self, arbiters):
+        row = 0 + self.offset_row
+        for arb in arbiters:
+            self.requests.append(input_data_into_cell(row, 0, "Arbiter"))
+            row += 1
+            self.offset_row += 1
+
+    #print the arb variables
+    #in order to do so, require to have an updated self.offset_row
+
+    def create_var_con(self, ctcr):
+        row = 0 + self.offset_row
+        for con in ctcr:
+            self.requests.append(input_data_into_cell(row, 0, "Consumer"))
+            row += 1
+            self.offset_row += 1
+
+    #get ready the info and the updated self.offset_row
+
+    def info_renderer(self):
+        self.requests.append(input_data_into_cell_merged(self.offset_row, self.offset_row + 1, 1, 25))
+        self.requests.append(input_data_into_cell(self.offset_row + 1, 1, "0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18"))
+
+    #how the process for each simulation looks like
+
+    def render_simulation(self, sim):
+
+        self.info_renderer()
+
+        #self.base_consumer_row = self.offset_row + 1 + len(arbiters) + 1
+
+
+        arbiters = self.assign_arbiter(sim)
+        ctcr = self.assign_colors_to_consumers(sim, arbiters)
+
+        for dp in sim:
+
+            #color arbiters
+            #order consumer based on arbiters
+
+            self.color_con_blocks(dp, ctcr)
 
     #make a loop that will color in each block depending on the line
 
-    def color_con_blocks(self, line):
-        column = int(line.cycle_number) + 1
-        if line.operation == 'io':
-            print(line.operation)
-            color = line.light_colors
-            print(color)
-            self.requests.append(update_cell_color(self.l_c_dict[color][0], self.l_c_dict[color][1], self.l_c_dict[color][2], self.row_index, self.row_index + 1, column, column + 1))
-        elif line.operation == 'work':
-            print(line.operation)
-            color = line.dark_colors
-            print(color)
-            self.requests.append(update_cell_color(self.d_c_dict[color][0], self.d_c_dict[color][1], self.d_c_dict[color][2], self.row_index, self.row_index + 1, column, column + 1))
+    def color_con_blocks(self, dp, ctcr):
 
+        if isinstance(dp, ConsumerDataPoint):
+
+            # get the color of a id from the ctcr list
+            values = ctcr.get(dp.consumer_id)
+            light_color = values[1]
+            dark_color = values[0]
+            column = int(dp.cycle_number) + 1
+
+            if dp.operation == 'io':
+
+                #color in the block
+                light_color = self.d_c_dict.get(light_color)
+                self.requests.append(update_cell_color(light_color[0], light_color[1], light_color[2], values[2], values[2] + 1, int(column), int(column) + 1))
+
+            elif dp.operation == 'work':
+
+                dark_color = self.l_c_dict.get(dark_color)
+                self.requests.append(update_cell_color(dark_color[0], dark_color[1], dark_color[2], values[2], values[2] + 1, int(column), int(column) + 1))
+
+    #first step in creating the spreadsheet
 
     def main_details_renderer(self):
         self.requests.append(get_clean_sheet_request())
         self.requests.append(unmerge_cells())
         self.requests.append(create_name_to_sheet("Generator"))
 
-    def info_renderer(self, sim):
-        self.offset_row += self.offset_row_add
-        self.requests.append(input_data_into_cell_merged(self.offset_row, self.offset_row + 1, 1, 25))
-        self.requests.append(input_data_into_cell(self.offset_row + 1, 1, "0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18"))
-        self.offset_row_add = len(sim) + 4
+    # process through the whole data /each sim rendering/ file and produce the info+blocks
+
+    def simulations_renderer(self):
+        for sim in self.sims:
+            self.render_simulation(sim)
 
     def render_sheet(self):
-        SheetRenderer.main_details_renderer(self)
-        SheetRenderer.simulation_renderer(self)
+        self.main_details_renderer()
+        self.simulations_renderer()
 
         spreadsheet_body = {
             'requests': self.requests
@@ -211,16 +248,10 @@ class ArbiterDataPoint(DataPoint):
 class ConsumerDataPoint(DataPoint):
 
     def __init__(self, consumer_id, operation, cycle_number):
-        self.consumer_id = ConsumerId(consumer_id)
+        self.consumer_id = consumer_id
         self.operation = operation
         self.cycle_number = cycle_number
 
-class ConsumerId():
-
-    def __init__(self, consumer_id):
-        self.consumer_id = consumer_id
-        self.dark_colors = None
-        self.light_colors = None
 
 def create_name_to_sheet(title):
     return {
@@ -266,8 +297,6 @@ def input_data_into_cell(rowIndex, columnIndex, data):
             }
         }
 
-def input_clock_data():
-    None
 
 def update_cell_color(red, green, blue, startRowIndex, endRowIndex, startColumnIndex,endColumnIndex):
     return {
