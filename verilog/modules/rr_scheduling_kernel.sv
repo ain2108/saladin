fdf764712a64500c59e3bd2571ea24e0b4892cb9
ain2108 <ain2108@columbia.edu>
2020-08-25 20:19:42 -0400

    sketch of the request selector logic

--- verilog/modules/rr_scheduling_kernel.sv
+++ verilog/modules/rr_scheduling_kernel.sv
@@ -8,12 +8,16 @@ module rr_scheduling_kernel #(
   parameter NPORTS = 1)
   (
   output reg [PLM_INPUT_WIDTH-1:0] out [NKERNELS], 
-  input [REQ_WIDTH-1:0] requests [NCONSUMERS],
+  output [PLM_INPUT_WIDTH-1:0] plm_inputs [NKERNELS], 
+  output [PLM_OUTPUT_WIDTH-1:0] plm_outputs [NKERNELS], 
+  input [REQ_WIDTH-1:0] requests [NCONSUMERS], 
   input clk, 
   input reset);
 
-  localparam REQ_WIDTH = ADDR_WIDTH + VALUE_WIDTH + 1 + 1;
-  localparam PLM_INPUT_WIDTH = (ADDR_WIDTH >> $clog2(NBANKS)) + VALUE_WIDTH + 1;
+  localparam REQ_WIDTH = ADDR_WIDTH + VALUE_WIDTH + 1 + 1; /* addr, value, wr, valid */
+  localparam NUM_BANK_BITS = $clog2(NBANKS);
+  localparam PLM_INPUT_WIDTH = (ADDR_WIDTH - NUM_BANK_BITS) + VALUE_WIDTH + 1;
+  localparam PLM_OUTPUT_WIDTH = VALUE_WIDTH;
 
   initial begin
     `assert_true((NPORTS == 1 || NPORTS == 2), "unspuported number of PLM ports")
@@ -50,7 +54,25 @@ module rr_scheduling_kernel #(
     /* There is a rr_pivot register for each port of each bank */
 		for (g_bank_i = 0; g_bank_i < NBANKS; g_bank_i = g_bank_i + 1) begin
       for (g_port_i = 0; g_port_i < NPORTS; g_port_i = g_port_i + 1) begin
-        
+
+        /* Determine validity of the candidate that the pivot is pointing to */
+        wire [REQ_WIDTH-1:0] lead_candidate; /* Bits of the request that pivot is pointing to */
+        wire [NUM_BANK_BITS-1:0] bank_address; /* Address bit of the said candidate */
+        wire is_candidate_addr_in_range; /* Is the address in range of the bank? */
+        wire is_valid_bit; /* Is the candidate valid bit set? */
+        wire is_eligible_request; /* Is this a request eligible for scheduling? */
+
+        assign lead_candidate = requests[rr_pivots[g_bank_i * NPORTS + g_port_i]];
+        assign bank_address = lead_candidate[REQ_WIDTH-1:REQ_WIDTH-NUM_BANK_BITS-1];
+
+        assign is_candidate_addr_in_range = (bank_address == g_bank_i);
+        assign is_valid_bit = lead_candidate[0];
+        assign is_eligible_request = is_candidate_addr_in_range && is_valid_bit;
+
+        assign plm_inputs[g_bank_i * NPORTS + g_port_i] = (is_eligible_request) ? 
+          lead_candidate[REQ_WIDTH-1:1] /* not including request valid bit */
+          : 0; /* All 0s is wr=0, this its a resource read */
+
         always @(posedge clk or posedge reset) begin
           if (reset) begin
 
